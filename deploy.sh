3e134a19ffe8428f90635a48b85cc1cd2baca185
ccchuhan <nywsdnk@gmail.com>
2022-05-30 23:08:15 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push chuhanfeng/multi-client:$SHA
 docker push chuhanfeng/multi-server:$SHA
 docker push chuhanfeng/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=chuhanfeng/multi-server:$SHA
-kubectl set image deployments/client-deployment client=chuhanfeng/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=chuhanfeng/multi-worker:$SHA
+#kubectl set image deployments/server-deployment server=chuhanfeng/multi-server:$SHA
+#kubectl set image deployments/client-deployment client=chuhanfeng/multi-client:$SHA
+#kubectl set image deployments/worker-deployment worker=chuhanfeng/multi-worker:$SHA
